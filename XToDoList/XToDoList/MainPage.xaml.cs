db68ff0b255b333679c539450048cb19301b65ac
mikhail <ronme@mail.ru>
2018-03-04 22:07:13 +0600

    Initial commit

---
+++ XToDoList/XToDoList/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XToDoList
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            ToDoItemsList itemlist = new ToDoItemsList();
+            ToDoList.BindingContext = itemlist;
+        }
+	}
+}
---
+++ XToDoList/XToDoList/ToDoItemsList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections.ObjectModel;
+
+namespace XToDoList
+{
+    public class ToDoItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public bool Copleted { get; set; }
+
+        public override string ToString()
+        {
+            return Title + " : " +
+                   Description + " [ " +
+                   CreationDate.ToShortDateString() + " ] " +
+                   Copleted.ToString();
+        }
+    }
+
+    public class ToDoItemsList
+    {
+        public ToDoItemsList()
+        {
+            toDoItems_.Add(new ToDoItem
+            {
+                Title = "Test1",
+                Description = "Test1Description",
+                CreationDate = DateTime.Now,
+                Copleted = false
+            });
+            toDoItems_.Add(new ToDoItem
+            {
+                Title = "Test2",
+                Description = "Test2Description",
+                CreationDate = DateTime.Now,
+                Copleted = true
+            });
+        }
+        private ToDoItem selecteItem_ = null;
+        public ToDoItem SelectedToDoItem { get { return selecteItem_; } set { selecteItem_ = value; } }
+        private IList<ToDoItem> toDoItems_ = new ObservableCollection<ToDoItem>();
+        public IEnumerable<ToDoItem> ToDoItems { get { return toDoItems_; } }
+    }
+}
